4293cccf1928445ad106bf6ff8a6f76a89ee6207
Deep <deepsingh.cts@gmail.com>
2019-10-29 22:04:21 +0530

    simple mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,46 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager',{
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
-    useCreateIndex: true
+    // useCreateIndex: true
+});
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+const me = new User({
+    name: 'Deep Singh',
+    age: 27
+});
+
+const myTask = new Task({
+    description: 'Prepare Google sheet for sync logs report.',
+    completed: false    
+});
+
+// me.save().then(()=>{
+//     console.log('me==', me)
+// }).catch((err)=>{
+//     console.log('Error: ', err)
+// });
+
+myTask.save().then(()=>{
+    console.log('task==', myTask)
+}).catch((err)=>{
+    console.log('Error: ', err)
 });
