7828ff1807471f5a08700eea9408d5e044e8523a
deepsingh <deepsingh.cts@gmail.com>
2020-06-10 02:46:43 +0530

    ceate APIs

--- src/index.js
+++ src/index.js
@@ -9,14 +9,23 @@ const port = process.env.PORT || 3000
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body);
-    user.save()
-        .then(() => {
-            res.send(user);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        })
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
 })
 
 app.listen(port, () => {
