5c8d5a89926533f2cce18f76d78d63e0ae26939e
deepsingh <deepsingh.cts@gmail.com>
2020-06-13 21:54:01 +0530

    update delete + refactoring

--- src/index.js
+++ src/index.js
@@ -1,79 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routes/user')
+const taskRouter = require('./routes/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json());
-
-//USER
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save();
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users)
-    }
-    catch (e) {
-        res.status(500).send();
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(204).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send();
-    }
-})
-
-//TASK
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send();
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const task = await Task.findById(_id);
-        if (!task) {
-            return res.status(204).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send();
-    }
-})
-
-app.post('/tasks', async (req, res) => {
-    try {
-        const task = new Task(req.body)
-        await task.save(task);
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log('Server is up on port ' + port);
 })
