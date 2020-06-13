ec1e4b8f5c93050dfc0cfbba8c8786ad55859b9c
deepsingh <deepsingh.cts@gmail.com>
2020-06-14 03:23:13 +0530

    added web tokens

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,12 @@ const User = require('../models/user')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     try {
-        await user.save()
-        res.status(201).send(user)
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e)
     }
@@ -16,7 +17,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
     } catch (e) {
         res.status(400).send();
     }
