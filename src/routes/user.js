a4b0beaf8d4feba6fbde7a8f29562b9cc28d8917
deepsingh <deepsingh.cts@gmail.com>
2020-06-14 02:06:04 +0530

    authentication

--- src/routes/user.js
+++ src/routes/user.js
@@ -48,15 +48,15 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
+        const user = await User.findById(req.params.id);
+        updates.forEach(update => user[update] = req.body[update]);
+        await user.save();
         if (!user) {
             return res.status(404).send()
         }
-
-        res.send(user)
+        res.send(user);
     } catch (e) {
-        res.status(400).send(e)
+        res.status(400).send(e);
     }
 })
 
