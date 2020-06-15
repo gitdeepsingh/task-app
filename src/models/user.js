70f060e47d10a5e8c7c382671ad9be5cf702e3f9
deepsingh <deepsingh.cts@gmail.com>
2020-06-16 02:38:15 +0530

    auth + remove all tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -49,6 +50,12 @@ const userSchema = mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.toJSON = function () {
     const user = this;
     userProfile = user.toObject();
@@ -90,6 +97,13 @@ userSchema.pre('save', async function (next) {
 
     next();
 });
+
+//delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id })
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
